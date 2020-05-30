516670f18fdbebdf369edd16b79f7f85bc7a8044
Alex S <alexspringer@pm.me>
2020-05-30 20:38:46 +0100

    separate dags for transaction and partenr

--- dags/dag.py
+++ dags/dag.py
@@ -17,7 +17,7 @@ c = toml.load('/usr/local/airflow/dags/config.toml')
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2020, 5, 29),
+    "start_date": datetime(2020, 5, 25),
     "email": ["alexspringer@pm.me"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -29,55 +29,63 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("network_update",
+partner_dag = DAG("partner_update",
             default_args=default_args,
             schedule_interval=timedelta(1))
 
-start_task = DummyOperator(task_id='start', dag=dag)
+start_task = DummyOperator(task_id='start', dag=partner_dag)
 
 awin_partners = BashOperator(
     task_id='awin_partner_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -p awin',
-    dag=dag
+    dag=partner_dag
 )
 
 admitad_partners = BashOperator(
     task_id='admitad_partner_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -p admitad',
-    dag=dag
+    dag=partner_dag
 )
 
 linkshare_partners = BashOperator(
     task_id='linkshare_partner_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -p linkshare',
-    dag=dag
+    dag=partner_dag
 )
+
+end_task = DummyOperator(task_id='end', dag=partner_dag)
+
+
+
+
+transaction_dag = DAG("transaction_update",
+            default_args=default_args,
+            schedule_interval=timedelta(1))
+
+start_transactions = DummyOperator(task_id='start_transactions', dag=transaction_dag)
 
 awin_transactions = BashOperator(
     task_id='awin_transaction_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -td {{ ds }} -s cross-network-asos --no_upload awin',
-    dag=dag
+    dag=transaction_dag
 )
 
 admitad_transactions = BashOperator(
     task_id='admitad_transaction_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -td {{ ds }} -s cross-network-asos --no_upload admitad',
-    dag=dag
+    dag=transaction_dag
 )
 
 linkshare_transactions = BashOperator(
     task_id='linkshare_transaction_update',
     bash_command='netimpact -c /usr/local/airflow/dags/config.toml -td {{ ds }} -s cross-network-asos --no_upload linkshare',
-    dag=dag
+    dag=transaction_dag
 )
 
 # TODO :: Modify action status in transactions based on modifications file
-end_task = DummyOperator(task_id='end', dag=dag)
+end_transactions = DummyOperator(task_id='end_transactions', dag=transaction_dag)
 
 
 
-start_task >> [awin_partners, admitad_partners, linkshare_partners]
-awin_partners >> awin_transactions
-admitad_partners >> admitad_transactions
-linkshare_partners >> linkshare_transactions
-[awin_transactions,admitad_transactions,linkshare_transactions] >> end_task
+start_task >> [awin_partners, admitad_partners, linkshare_partners] >> end_task
+start_transactions >> [awin_transactions,admitad_transactions,linkshare_transactions] >> end_transactions
